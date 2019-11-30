ec991cfc9ba35ca7ba4293b09b7ae0f7878cb3ea
Taleb_84 <taleb_n1@hotmail.com>
2019-11-30 16:52:24 +0100

    render data dynamically
    modify app.js and index.ejs
    to rander dynamically data.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const http = require('http');
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 app.use('/static', express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/:userQuery', function(req, res){
+app.get('/:userQuery', function(req, ejs){
     // res.sendFile(path.join(__dirname, '/index.html'));
-    res.render('index', {data : {userQuery: req.params.userQuery}});
+    ejs.render('index', {data : {userQuery: req.params.userQuery, 
+                        searchResults: ['book1', 'book2', 'book3']}});    
 });
 app.listen(8080);
