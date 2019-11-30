8c66c85f51c5f038655b4677cf03336ae0edafc6
Taleb_84 <taleb_n1@hotmail.com>
2019-11-30 16:39:08 +0100

    create index.ejs,
    create template index.ejs
    modify app.js and render index.ejs

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const path = require('path');
 
 const app = express();
 app.use('/static', express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '/index.html'));
+    // res.sendFile(path.join(__dirname, '/index.html'));
+    res.render('index');
 });
 app.listen(8080);
