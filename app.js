9049398f8eec07b0ab2810361cc563f123266bff
Taleb_84 <taleb_n1@hotmail.com>
2019-11-30 16:45:48 +0100

    first usage of ejs
    modify app.js and render the template with ejs.
    install ejs
    modify the index.ejs and render dynamic data.

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express();
 app.use('/static', express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
+app.get('/:userQuery', function(req, res){
     // res.sendFile(path.join(__dirname, '/index.html'));
-    res.render('index');
+    res.render('index', {data : {userQuery: req.params.userQuery}});
 });
 app.listen(8080);
